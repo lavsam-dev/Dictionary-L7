8d4a3bf5501d5ca2e114311b309800c2523720df
lavsam-dev <lavsam@gmail.com>
2021-09-21 12:01:37 +0400

    Properties.kt with Example Делегирование

--- app/src/main/java/geekbrains/ru/translator/view/Properties.kt
+++ app/src/main/java/geekbrains/ru/translator/view/Properties.kt
@@ -1,15 +1,19 @@
 package geekbrains.ru.translator.view
 
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
+import kotlin.reflect.KProperty
 
 class Example {
-   var name: String
-   get() = "123"
-    set(value) {
-
-    }
+    private var localName = ""
+    var name: String
+        get() = "123"
+        set(value) {
+            Log.d("Example", "Set name $localName = $value")
+            localName = value
+        }
 
     var surname: String = "Surname"
 }
@@ -19,15 +23,34 @@ class Properties : AppCompatActivity() {
     private val propertyDelegate = PropertyDelegate<Properties>()
 
 
-//    var name by PropertyDelegate()
+    var name by PropertyDelegate()
 
     val vm by viewModel<ViewModel>()
 }
 
-class PropertyDelegate<T>
+class PropertyDelegate<T> {
+
+    private var holder: T? = null
+
+    operator fun getValue(properties: T, property: KProperty<*>): Any {
+        Log.d("PropertyDelegate", "Get value = $holder")
+        return holder as Any
+    }
+
+    operator fun setValue(properties: T, property: KProperty<*>, any: Any) {
+        Log.d("PropertyDelegate", "Set value $holder = $any")
+        holder = any as T
+    }
+}
 
 fun main() {
     val example = Example()
 
     example.surname = "321"
+    print(example.surname)
+
+    val p = Properties()
+
+    p.name = "123"
+    print(p.name)
 }
