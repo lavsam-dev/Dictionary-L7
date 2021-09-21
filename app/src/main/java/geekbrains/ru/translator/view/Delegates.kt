32c661ee2c4df7e8e05075e013c0c34a9f9dd4c5
lavsam-dev <lavsam@gmail.com>
2021-09-21 11:09:45 +0400

    дублирование кода

--- app/src/main/java/geekbrains/ru/translator/view/Delegates.kt
+++ app/src/main/java/geekbrains/ru/translator/view/Delegates.kt
@@ -16,22 +16,40 @@ interface IDuck {
 
 open class Car : Icar {
     override fun drive() {
-
+        // car code
     }
 }
 
 open class Boat : IBoat {
     override fun swim() {
-
+        // boat code
     }
 }
 
 open class Duck : IDuck {
     override fun krya() {
-
+        // krya code
     }
 
     override fun swim() {
+        // swim code
+    }
+}
 
+class Monster(
+    private val car: Car,
+    private val boat: Boat,
+    private val duck: Duck
+) : Icar, IBoat, IDuck {
+    override fun drive() {
+        // car code
+    }
+
+    override fun krya() {
+        // krya code
+    }
+
+    override fun swim() {
+        // boat code
     }
 }
